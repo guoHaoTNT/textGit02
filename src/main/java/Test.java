edd6f8b0c17480f81a7ef638f52e72329daa61eb
guoHaoTNT <63487275+guoHaoTNT@users.noreply.github.com>
2020-04-12 17:40:36 +0800

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,6 @@ import javax.xml.bind.SchemaOutputResolver;
 public class Test {
     public static void main(String[] args) {
         System.out.println("分支的测试！！！");
+          System.out.println("从git上克隆到软件中去");
     }
 }
