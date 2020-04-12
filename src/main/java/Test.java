ab3a99d7440cae8ea810b79d69e24be4014237fc
1213 <1213@atguugu.com>
2020-04-12 17:51:15 +0800

    push 提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,10 @@
+import jdk.management.resource.internal.inst.SocketOutputStreamRMHooks;
+
 import javax.xml.bind.SchemaOutputResolver;
 
 public class Test {
     public static void main(String[] args) {
         System.out.println("分支的测试！！！");
+        System.out.println("推送，push测试");
     }
 }
